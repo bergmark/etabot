4dbbfbc476dffc325840303590600db78cf56355
Adam Bergmark <adam@edea.se>
2012-08-21 15:55:38 +0200

    updates for #fay

--- src/EtaBot/Main.hs
+++ src/EtaBot/Main.hs
@@ -2,16 +2,23 @@
 
 module Main where
 
-import           IO
 import           Control.Monad
-import qualified Data.ByteString as B
+import qualified Data.ByteString    as B
 import           Data.Maybe
-import qualified Data.Text as T
+import qualified Data.Text          as T
 import           Data.Text.Encoding
 import           Network.SimpleIRC
+import           System.IO
 
 botName :: String
-botName = "etabot"
+botName = "faybot"
+
+checkChans :: [B.ByteString]
+checkChans = ["#haskell","#snapframework"]
+sendChan :: B.ByteString
+sendChan = "#fay"
+ignoreSpeakers :: [B.ByteString]
+ignoreSpeakers = ["etabot"]
 
 config :: IrcConfig
 config = defaultConfig
@@ -20,20 +27,21 @@ config = defaultConfig
     , cNick = botName
     , cUsername = botName
     , cRealname = botName
-    , cChannels = ["#snapframework", "#haskell"]
+    , cChannels = ["#fay-test1", "#fay-test2", "#fay-test3"]
     , cEvents = [notifySnap]
     }
 
-onDecodeError descr input = Nothing
 
+onDecodeError :: a -> b -> Maybe c
+onDecodeError _ _ = Nothing
+
+subst :: Eq a => [a] -> [a] -> [a] -> [a]
 subst _    _  [       ] = []
 subst from to xs@(a:as) =
     if isPrefixOf from xs
         then to ++ subst from to (drop (length from) xs)
         else a : subst from to as
-    where isPrefixOf as bs = and $ zipWith (==) as bs
-
-foo = T.concat . T.splitOn "snapshot" 
+    where isPrefixOf as' bs = and $ zipWith (==) as' bs
 
 ------------------------------------------------------------------------------
 notifySnap :: IrcEvent
@@ -44,21 +52,18 @@ notifySnap = Privmsg $ \mirc imsg -> do
         chan = fromMaybe "<unknown>" (mChan imsg)
         checkNotify str = when (str `T.isInfixOf` msg) $ do
             let out = B.concat
-                    [ speaker 
-                    , " is talking about " 
-                    , encodeUtf8 str 
-                    , " in " 
-                    , chan 
-                    , ": " 
+                    [ speaker
+                    , " is talking about "
+                    , encodeUtf8 str
+                    , " in "
+                    , chan
+                    , ": "
                     , encodeUtf8 actualMsg
                     ]
-            sendMsg mirc "#snapframework" out
+            sendMsg mirc sendChan out
             putStrLn $ T.unpack $ decodeUtf8 out
-    if chan == "#haskell"
-      then do checkNotify "snap"
-              checkNotify "heist"
-              checkNotify "xmlhtml"
-      else return ()
+    when (chan `elem` checkChans && not (speaker `elem` ignoreSpeakers))
+          (checkNotify "fay")
 
 main :: IO ()
 main = do
